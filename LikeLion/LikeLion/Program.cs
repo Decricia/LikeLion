e2f75e8b4466256ff1f053d75153e8cd4a645ec7
김승연 <98165542+Decricia@users.noreply.github.com>
2025-02-21 17:27:02 +0900

    공부

--- LikeLion/LikeLion/Program.cs
+++ LikeLion/LikeLion/Program.cs
@@ -10,8 +10,60 @@ namespace LikeLion
     {
         static void Main(string[] args)
         {
-            //출력
-            Console.WriteLine("깃허브 연결하기");
+
+            float ruinDamage = 0.0f;
+            float cardGauge = 0.0f;
+            float ultDamage = 0.0f;
+            float maxMP = 0.0f;
+            float recoveryMP = 0.0f;
+            float idleRecoveryMP = 0.0f;
+            float moveSpeed = 0.0f;
+            float rideSpeed = 0.0f;
+            float transSpeed = 0.0f;
+            float coolDown = 0.0f;
+
+            Console.Write("루인 스킬 피해를 입력해주세요. : ");
+            ruinDamage = float.Parse(Console.ReadLine());
+
+            Console.Write("카드 게이지 획득량을 입력하세요 : ");
+            cardGauge = float.Parse(Console.ReadLine());
+
+            Console.Write("각성기 피해를 입력하세요. : ");
+            ultDamage = float.Parse(Console.ReadLine());
+
+            Console.Write("최대 마나를 입력하세요. : ");
+            maxMP = float.Parse(Console.ReadLine());
+
+            Console.Write("전투 중 마나 회복량을 입력하세요. : ");
+            recoveryMP = float.Parse(Console.ReadLine());
+
+            Console.Write("비 전투 중 마나 회복량을 입력하세요. : ");
+            idleRecoveryMP = float.Parse(Console.ReadLine());
+
+            Console.Write("이동속도를 입력하세요. : ");
+            moveSpeed = float.Parse(Console.ReadLine());
+
+            Console.Write("탈 것 속도를 입력하세요. : ");
+            rideSpeed = float.Parse(Console.ReadLine());
+
+            Console.Write("운반속도를 입력하세요. : ");
+            transSpeed = float.Parse(Console.ReadLine());
+
+            Console.Write("스킬 재사용 대기시간을 입력하세요. : ");
+            coolDown = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine($"루인 스킬 피해 :    {ruinDamage}%");
+            Console.WriteLine($"카드 게이지 획득량 :    {cardGauge}%");
+            Console.WriteLine($"각성기 피해 :    {ultDamage}%");
+            Console.WriteLine($"최대 마나 :    {maxMP}");
+            Console.WriteLine($"전투 중 마나 회복량 :    {recoveryMP}");
+            Console.WriteLine($"비 전투 중 마나 회복량 :    {idleRecoveryMP}");
+            Console.WriteLine($"이동속도 :    {moveSpeed}%");
+            Console.WriteLine($"탈 것 속도 :    {rideSpeed}%");
+            Console.WriteLine($"운반속도 :    {transSpeed}%");
+            Console.WriteLine($"스킬 재사용 대기시간 감소 :    {coolDown}%");
         }
     }
 }
---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩화면 만들기
+            //게임스토리 1
+            Console.WriteLine("Loading.");
+            Console.WriteLine("■□□□□□□□");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine("Loading..");
+            Console.WriteLine("■■□□□□□□");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine("Loading...");
+            Console.WriteLine("■■■□□□□");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine("Loading..");
+            Console.WriteLine("■■■■□□□");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine("Loading.");
+            Console.WriteLine("■■■■■□□");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine("Loading..");
+            Console.WriteLine("■■■■■■□");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine("Loading...");
+            Console.WriteLine("■■■■■■■");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine("Press Enter to Start.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("멀리서 화살 소리 하나가 지나쳐간다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" '퍽' 무언가에 꽂힌듯 더이상 바람을 가르는 소리가 들리지 않는다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("그리고 들려오는 환호소리와 대화소리에 무심코 귀를 기울여본다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("'*^&#%#$ 라니까~!'.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("(역시나 거리가 멀어 그런지 제대로 들리지 않는다 좀 더 가까이 가봐야겠다.)");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("'역시 왕도의 궁술 1위 #$% 답네 감탄만 나오는 실력입니다!'.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("어느정도 가까이 가자 누군가의 이름만을 제외한체 대충 상황을 파악하였다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("궁금증이 해결되어 다시 돌아가려던 찰나.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("'콰직' 하는 나뭇가지가 부러지는 소리와 함께 사람들의 대화소리가 멈추었다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("나때문인가 싶어 뒤를 돌아보려던 순간.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("무언가가 바람을 가르며 빠르게 날아오는 소리가 들렸다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(" '퍽' ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("순간의 고통이 몸이 뜨겁게 타오르고 목소리가 나오지 않게 되었다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("이윽고 사람들의 환호소리와 대화소리가 다시 들려온다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("'와 오늘만 벌써 열 두명째입니다!'.");
+            Console.WriteLine("''이렇게 가면 #@$님이 우승이십니다!");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("왜 아직도 저 이름이 익숙치 않지 라는 생각이 들며 서서히 졸음이 몰려왔다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("기회가 된다면 나도 다음생에는 사냥꾼으로 태어나고싶다.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("이제 더 이상 졸음을 참을 수 없다");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("아 맞다 그의 이름은");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(".....................");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Press Enter to Restart");
+            Console.WriteLine("〓〓〓〓〓〓〓〓〓〓");
+            Thread.Sleep(300);
+            Console.ReadLine();
+            Console.Clear();
+            
+            Console.WriteLine("Press Enter to Restart");
+            Console.WriteLine("〓〓〓〓〓〓〓〓〓〓▶");
+            Thread.Sleep(300);
+            Console.ReadLine();
+            Console.Clear(); 
+
+            Console.WriteLine("Press Enter to Restart");
+            Console.WriteLine("〓〓〓〓라이덴〓〓〓▶");
+            Thread.Sleep(300);
+            Console.ReadLine();
+            Console.Clear();
+
+            /*Console.WriteLine("Loading..");
+            Console.WriteLine("■■■■■■□");
+            Thread.Sleep(500);
+            Console.Clear();*/
+        }
+    }
+}
